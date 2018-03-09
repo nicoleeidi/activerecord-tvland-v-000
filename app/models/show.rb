266c30ef4a64424011f85acab3b8422203fada66
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-09 16:28:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   #needs genre= method
-  def build_network
-    self.network.call_letters
+  def build_network(network)
+    self.network.call_letters = network 
   end
 end
