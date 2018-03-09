68bd28b5a198ff8f0654f975cddc56179959ba8e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-09 16:34:16 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   #needs genre= method
-  def build_network(network)
-    binding.pry
-    self.network.call_letters = network
-  end
+  # def build_network(network)
+  #   binding.pry
+  #   self.network.call_letters = network
+  # end
 end
