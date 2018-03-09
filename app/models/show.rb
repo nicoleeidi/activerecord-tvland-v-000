ae6a6c2b69f5b32e43201c6ae28d53826f15ee22
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-09 16:31:28 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
   #needs genre= method
   def build_network(network)
-    self.network.call_letters = network
+    binding.pry
+    self.network.call_letters = network 
   end
 end
