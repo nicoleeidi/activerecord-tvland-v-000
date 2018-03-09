be81330be28ec59cff6a5cfe0963bd63eb212c9f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-09 16:12:59 +0000

    automatically backed up by learn

--- db/migrate/007_add_stuff_to_shows.rb
+++ db/migrate/007_add_stuff_to_shows.rb
@@ -2,7 +2,7 @@ class AddStuffToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :genre, :string
     add_column :shows, :day, :string
-    add_column :shows, :season :string
+    add_column :shows, :season, :string
 
 end
 end
